98d5f4e8015d68b14933b3d802fd6924ce3bdcb3
COMP3100 Rhythm Sood <rhythm.sood@students.mq.edu.au>
2022-03-24 18:01:16 +1100

    Refactored the client, added while loop

--- src/MyClient.java
+++ src/MyClient.java
@@ -3,28 +3,37 @@ import java.net.*;
 
 public class MyClient {  
     public static void main(String[] args) {  
-        try{      
+        try{  
             Socket s=new Socket("localhost",50000);  
             BufferedReader dis=new BufferedReader(new InputStreamReader(s.getInputStream())); //reads client message
-            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  //sends a message
-            // String username = System.getProperty("Rhythm");
-            // sending 1st message
-            dout.write(("HELO\n").getBytes());  
-            dout.flush();  
-            // reading 1st server message
-            String  str=(String)dis.readLine();  
-            System.out.println("message= "+str);
-            // dout.write(("AUTH Rhythm\n").getBytes());  
-            // dout.flush();
-            // // sending 2nd message
-            // dout.write(("REDY\n").getBytes());
-            // dout.flush(); 
-            // //reading 2nd message
-            // String  str3=(String)dis.readLine();  
-            // System.out.println("message= "+str3); 
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  //sends a message 
+
+            jobschd(dis, dout, "HELO\n", 1);
+            jobschd(dis, dout, "AUTH Rhythm\n", 1);
+            jobschd(dis, dout, "REDY\n", 1);
+            String nRecs = jobschd(dis, dout, "GETS All\n", 1);
+            jobschd(dis, dout, "OK\n", Integer.parseInt(nRecs));
+
             dout.close();
-            s.close();  
+            s.close(); 
         }
         catch(Exception e){System.out.println(e);}  
+    }
+
+    public static String jobschd(BufferedReader dis, DataOutputStream dout, String message, int repeat) throws UnknownHostException, IOException {
+        int i = 1;
+        String nRecs = "";
+        while(i <= repeat) {
+            dout.write((message).getBytes());  
+            dout.flush();
+            String  str=(String)dis.readLine();
+            if(str.startsWith("DATA")) {
+                nRecs = str.substring(str.indexOf(' ')+1, str.lastIndexOf(' '));  
+                return nRecs;
+            }
+            System.out.println(str);
+            i++;
+        }
+        return "";
     }  
 }  
