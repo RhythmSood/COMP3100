973caae7391c49e854aefdc8791456b0b912e649
COMP3100 Rhythm Sood <rhythm.sood@students.mq.edu.au>
2022-03-28 12:30:41 +1100

    xmlParser added and all jobs schdeluer

--- src/MyClient.java
+++ src/MyClient.java
@@ -3,37 +3,39 @@ import java.net.*;
 
 public class MyClient {  
     public static void main(String[] args) {  
+         xmlReader handler = new xmlReader();
+         System.out.println(handler.bigServer());
         try{  
             Socket s=new Socket("localhost",50000);  
             BufferedReader dis=new BufferedReader(new InputStreamReader(s.getInputStream())); //reads client message
             DataOutputStream dout=new DataOutputStream(s.getOutputStream());  //sends a message 
 
-            jobschd(dis, dout, "HELO\n", 1);
-            jobschd(dis, dout, "AUTH Rhythm\n", 1);
-            jobschd(dis, dout, "REDY\n", 1);
-            String nRecs = jobschd(dis, dout, "GETS All\n", 1);
-            jobschd(dis, dout, "OK\n", Integer.parseInt(nRecs));
-
+            jobschd(dis, dout, "HELO\n");
+            jobschd(dis, dout, "AUTH Rhythm\n");
+            String job = jobschd(dis, dout, "REDY\n");
+            while(!job.equals("NONE")) {
+                if(job.startsWith("JCPL")) {
+                    jobschd(dis, dout, "REDY\n");
+                } else if(job.startsWith("JOBN")){
+                    String temp = job.substring(job.indexOf(' ')+1);
+                    temp = temp.substring(temp.indexOf(' ')+1);
+                    String jobID = temp.substring(0, temp.indexOf(' '));
+                    jobschd(dis, dout, "SCHD "+jobID+" "+handler.bigServer().get(0)+" 0\n");
+                }
+                job = jobschd(dis, dout, "REDY\n");
+            }
+            
             dout.close();
             s.close(); 
         }
         catch(Exception e){System.out.println(e);}  
     }
 
-    public static String jobschd(BufferedReader dis, DataOutputStream dout, String message, int repeat) throws UnknownHostException, IOException {
-        int i = 1;
-        String nRecs = "";
-        while(i <= repeat) {
-            dout.write((message).getBytes());  
-            dout.flush();
-            String  str=(String)dis.readLine();
-            if(str.startsWith("DATA")) {
-                nRecs = str.substring(str.indexOf(' ')+1, str.lastIndexOf(' '));  
-                return nRecs;
-            }
-            System.out.println(str);
-            i++;
-        }
-        return "";
+    public static String jobschd(BufferedReader dis, DataOutputStream dout, String message) throws UnknownHostException, IOException {
+        dout.write((message).getBytes());  
+        dout.flush();
+        String  str=(String)dis.readLine();
+        System.out.println(str);
+        return str;
     }  
 }  
