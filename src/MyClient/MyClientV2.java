3a924a32b67fac056c337dc42294b7c283441763
Rhythm Sood <rhythm.sood@students.mq.edu.au>
2022-05-29 13:40:49 +1000

    added a custom fastest algo

--- src/MyClient/MyClientV2.java
+++ src/MyClient/MyClientV2.java
@@ -1,43 +1,18 @@
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import DataExtracter.jobinfo;
 import DataExtracter.serverdata;
 
 public class MyClientV2 {
 
-    private static jobinfo currentJob; // stores info on the current job that requires scheduling
-    private static int currentServer = 0; // stores the index of the server that should receive the next job scheduled
+    private static jobinfo currentJob; // stores info of the current job
 
-    private static serverdata[] servers; // stores the hardware information of all the servers as sent by ds-server
-    private static List<serverdata> largestServers = new ArrayList<serverdata>(); // a list of all servers of the
-                                                                                  // largest type
-
-    private static String[] globalArgs; // global copy of command line arguments for use in utility methods
-
-    private static final int DEFAULT_PORT = 50000;
     private static final int MAX_RUNTIME = 1000;
 
     public static void main(String[] args) {
         try {
-            // set args to avoid null issues with later switch statements
-            if (args.length == 0) {
-                args = new String[2];
-                args[0] = "blank";
-                args[1] = "blank";
-            } else if (args.length == 1) {
-                String holder = args[0];
-                args = new String[2];
-                args[0] = holder;
-                args[1] = "blank";
-            }
-
-            globalArgs = args;
-
             // open the socket used to connect to the server
-            Socket socket = new Socket("localhost", DEFAULT_PORT);
+            Socket socket = new Socket("localhost", 50000);
 
             // initialise for input and output
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -46,14 +21,14 @@ public class MyClientV2 {
             // send HELO to server
             sendMessage("HELO", out);
 
-            // wait for OK from server
-            waitFor("OK", in);
+            // recieves OK from server
+            receiveMessage(in);
 
             // send AUTH and authentication info to server
             sendMessage("AUTH" + " " + System.getProperty("user.name"), out);
 
-            // wait for OK from server
-            waitFor("OK", in);
+            // recieves OK from server
+            receiveMessage(in);
 
             // send REDY when ready to start reading jobs
             sendMessage("REDY", out);
@@ -62,36 +37,8 @@ public class MyClientV2 {
             String msg = receiveMessage(in);
             currentJob = extractJobInfo(msg);
 
-            // initialise server info array
-            servers = getServersData(in, out, "all");
-
-            // space output
-            System.out.println();
-
-            // get list of largest servers for use in scheduling
-            largestServers = getLargestServers();
-
             // schedule first job
-            switch (args[0]) {
-                case "-fc":
-                    scheduleJobFc(in, out);
-                    break;
-                case "-lrr":
-                    scheduleJobLrr(out);
-                    break;
-                case "-lb":
-                    scheduleToLeastBusy(in, out);
-                    break;
-                case "-fcl":
-                    scheduleJobFcLimited(in, out);
-                    break;
-                case "-sw":
-                    scheduleJobShortestWait(in, out);
-                    break;
-                default:
-                    scheduleJobCustom(in, out);
-                    break;
-            }
+            scheduleJobCustom(in, out);
 
             // wait for OK
             receiveMessage(in);
@@ -104,9 +51,6 @@ public class MyClientV2 {
 
             // main loop, handles all messages from the server from now on
             while (moreJobs) {
-                // space console output for readability
-                System.out.println();
-
                 // send REDY for next info
                 sendMessage("REDY", out);
 
@@ -122,25 +66,7 @@ public class MyClientV2 {
                         // schedule the job
                         currentJob = extractJobInfo(msg);
 
-                        switch (args[0]) {
-                            case "-fc":
-                                scheduleJobFc(in, out);
-                                break;
-                            case "-lrr":
-                                scheduleJobLrr(out);
-                            case "-lb":
-                                scheduleToLeastBusy(in, out);
-                                break;
-                            case "-fcl":
-                                scheduleJobFcLimited(in, out);
-                                break;
-                            case "-sw":
-                                scheduleJobShortestWait(in, out);
-                                break;
-                            default:
-                                scheduleJobCustom(in, out);
-                                break;
-                        }
+                        scheduleJobCustom(in, out);
 
                         receiveMessage(in);
                         break;
@@ -148,23 +74,6 @@ public class MyClientV2 {
                         // there are no more jobs so stop the loop
                         moreJobs = false;
                         break;
-                    case "JCPL":
-                        String[] split = msg.split(" ");
-                        switch (args[0]) {
-                            case "-lb":
-                                // decrement jobs for appropriate server
-                                servers[getServerIndexByInfo(split[3], split[4])].jobs--;
-                                break;
-                            case "-sw":
-                                int serverIndex = getServerIndexByInfo(split[3], split[4]);
-                                servers[serverIndex].estCompletionTimes.set(0,
-                                        servers[serverIndex].estCompletionTimes.get(0)
-                                                - servers[serverIndex].estCompletionTimes.get(1));
-                                servers[serverIndex].estCompletionTimes.remove(1);
-                                break;
-                            default:
-                                break;
-                        }
                     default:
                         break;
                 }
@@ -177,39 +86,6 @@ public class MyClientV2 {
 
             out.close();
             socket.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    /////////// SCHEDULING METHODS ////////////
-
-    // schedules jobs according to a largest round robin algorithm
-    private static void scheduleJobLrr(DataOutputStream out) {
-        try {
-            // send scheduling request
-            sendMessage("SCHD " + currentJob.id + " " + largestServers.get(currentServer).type + " "
-                    + largestServers.get(currentServer).id, out);
-
-            // move to next server for lrr scheduling
-            if (currentServer == largestServers.size() - 1)
-                currentServer = 0;
-            else
-                currentServer++;
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    // schedules jobs according to a first capable algorithm
-    private static void scheduleJobFc(BufferedReader in, DataOutputStream out) {
-        try {
-            // get first capable server
-            serverdata capableInfo = getServersData(in, out, "capable")[0];
-
-            // send scheduling request
-            sendMessage("SCHD " + currentJob.id + " " + capableInfo.type + " " + capableInfo.id, out);
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -260,125 +136,6 @@ public class MyClientV2 {
         }
     }
 
-    // schedule jobs to the capable server with the least total jobs (i.e. waiting
-    // jobs + running jobs)
-    private static void scheduleToLeastBusy(BufferedReader in, DataOutputStream out) {
-        try {
-            int currentJobs = 1;
-            int minJobs = 10000;
-            int scheduleTo = 0;
-            int i = 0;
-
-            // find the capable server with the least jobs based on local data
-            do {
-                // check that server is capable of running job
-                if (servers[i].cores >= currentJob.reqCores && servers[i].memory >= currentJob.reqMem
-                        && servers[i].disk >= currentJob.reqDisk) {
-                    currentJobs = servers[i].jobs;
-                    // if server has less jobs waiting than current min, make it min
-                    if (currentJobs < minJobs) {
-                        minJobs = currentJobs;
-                        scheduleTo = i;
-                    }
-                }
-                i++;
-            } while (minJobs > 0 && i < servers.length);
-
-            // send scheduling request
-            sendMessage("SCHD " + currentJob.id + " " + servers[scheduleTo].type + " " + servers[scheduleTo].id, out);
-
-            // increment jobs for server that recieved jobs
-            servers[scheduleTo].jobs++;
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    private static void scheduleJobFcLimited(BufferedReader in, DataOutputStream out) {
-        String rply;
-
-        // get capable servers
-        serverdata[] capServers = getServersData(in, out, "capable");
-
-        // find the first capable server with an estimated runtime under the threshold
-        int index = 0;
-        int currentEstRuntime = 0;
-        do {
-            // get total estimate runtime for server
-            sendMessage("EJWT " + capServers[index].type + " " + capServers[index].id, out);
-
-            rply = receiveMessage(in);
-
-            currentEstRuntime = Integer.valueOf(rply);
-
-            index++;
-        } while (currentEstRuntime > MAX_RUNTIME && index < capServers.length);
-
-        // decrement index by one as it is incremented regardless of whether loop will
-        // continue
-        index--;
-
-        // send scheduling request
-        sendMessage("SCHD " + currentJob.id + " " + capServers[index].type + " " + capServers[index].id, out);
-    }
-
-    private static void scheduleJobShortestWait(BufferedReader in, DataOutputStream out) {
-        try {
-            // find the first capable server with an estimated runtime under the threshold
-            int i = 0;
-            int currentWait = 0;
-            int minWait = 10000000;
-            int scheduleTo = 0;
-
-            do {
-                currentWait = 0;
-                // check that server is capable of running job
-                if (servers[i].cores >= currentJob.reqCores && servers[i].memory >= currentJob.reqMem
-                        && servers[i].disk >= currentJob.reqDisk) {
-                    /*System.out.println("server matches with " + servers[i].cores + " " + servers[i].memory + " "
-                            + servers[i].disk + " against " + currentJob.reqCores + " " + currentJob.reqMem + " "
-                            + currentJob.reqDisk);*/
-                    currentWait = servers[i].estCompletionTimes.get(0);
-                    //System.out.println("est wait time for server: " + currentWait);
-                    if (currentWait < minWait) {
-                        minWait = currentWait;
-                        scheduleTo = i;
-                    }
-                }
-                i++;
-            } while (minWait > 0 && i < servers.length);
-
-            // decrement index by one as it is incremented regardless of whether loop will
-            // continue
-            i--;
-
-            // send scheduling request
-            sendMessage("SCHD " + currentJob.id + " " + servers[scheduleTo].type + " " + servers[scheduleTo].id, out);
-            servers[scheduleTo].estCompletionTimes
-                    .add(currentJob.estRunTime);
-            servers[scheduleTo].estCompletionTimes.set(0,
-                    servers[scheduleTo].estCompletionTimes.get(0) + currentJob.estRunTime);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    /////////// UTILITY METHODS ////////////
-
-    // waits until the specified message is received
-    private static void waitFor(String msg, BufferedReader in) {
-        String input = "";
-        try {
-            while (!input.equals(msg)) {
-                input = in.readLine();
-            }
-            System.out.println("Received: " + input);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
     // send a message to the server
     private static void sendMessage(String msg, DataOutputStream out) {
         try {
@@ -394,11 +151,8 @@ public class MyClientV2 {
     // receive message
     private static String receiveMessage(BufferedReader in) {
         try {
-            String rply = in.readLine();
-            // output to console
-            System.out.println("Received: " + rply);
-
-            return rply;
+            String reply = in.readLine();
+            return reply;
         } catch (Exception e) {
             System.out.println(e);
             return null;
@@ -417,36 +171,6 @@ public class MyClientV2 {
         String[] info = msg.split(" ");
         serverdata server = new serverdata(info[0], info[1], info[4], info[5], info[6]);
         return server;
-    }
-
-    // get the position of a server within the local servers array based on server
-    // type and id
-    private static int getServerIndexByInfo(String type, String id) {
-        for (int i = 0; i < servers.length; i++) {
-            if (servers[i].type.equals(type) && servers[i].id.equals(id)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    // returns a list of all servers that are of the type with most CPU cores
-    private static ArrayList<serverdata> getLargestServers() {
-        int largestIndex = 0;
-        // determine which server type has the most CPU cores; if more than one, take
-        // the first
-        for (int i = 0; i < servers.length; i++) {
-            if (servers[i].cores > servers[largestIndex].cores)
-                largestIndex = i;
-        }
-        // add all servers of the largest type to the largestType list
-        String largestType = servers[largestIndex].type;
-        ArrayList<serverdata> largest = new ArrayList<serverdata>();
-        for (serverdata server : servers) {
-            if (server.type.equals(largestType))
-                largest.add(server);
-        }
-        return largest;
     }
 
     private static serverdata[] getServersData(BufferedReader in, DataOutputStream out, String mode) {
@@ -501,15 +225,5 @@ public class MyClientV2 {
         receiveMessage(in);
 
         return servers;
-    }
-
-    private static void log(Object s) {
-        String arg = globalArgs[1];
-        if (arg.equals("-l")) System.out.println(s.toString());
-    }
-
-    private static void log() {
-        String arg = globalArgs[1];
-        if (arg.equals("-l")) System.out.println();
     }
 }
